defe4e1254feb3b7935e43ec9519a4492b5c0861
wjp <1060638479@qq.com>
2025-07-30 22:33:08 +0800

    [200~前端初始化完成

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// ant-design-vue
+
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // ant-design-vue
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
