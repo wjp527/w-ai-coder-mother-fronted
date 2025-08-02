9126d75b51bda4c64e32f94e462d7ba96bb621df
wjp <1060638479@qq.com>
2025-08-02 14:51:25 +0800

    feat: 前端完成用户管理模块的开发

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import ACCESS_ENUM from '@/access/accessEnum'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -5,18 +6,44 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: '主页',
       component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      name: '关于',
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/user/login',
+      name: '用户登录',
+      component: () => import('../views/user/UserLogin.vue'),
+    },
+    {
+      path: '/user/register',
+      name: '用户注册',
+      component: () => import('../views/user/UserRegister.vue'),
+    },
+    {
+      path: '/admin/userManage',
+      name: '用户管理',
+      component: () => import('../views/admin/UserManageView.vue'),
+      meta: {
+        access: ACCESS_ENUM.ADMIN
+      }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: '找不到页面',
+      component: () => import('../views/noAuth/index.vue'),
+      meta: {
+        access: ACCESS_ENUM.NOT_LOGIN
+      }
+    }
   ],
+
 })
+
+
 
 export default router
